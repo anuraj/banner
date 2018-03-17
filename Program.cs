ad938ebc3e8580ccee06e437280ea461d9ef8fe6
Anuraj <anuraj.p@msn.com>
2018-03-17 22:51:07 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Figgle;
+
+namespace Banner
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length >= 1)
+            {
+                var text = string.Empty;
+                foreach (var arg in args)
+                {
+                    text += $" {arg}";
+                }
+                Console.WriteLine(FiggleFonts.Standard.Render(text));
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine("Error : Text option missing. Hello <text>");
+                return -1;
+            }
+        }
+    }
+}
